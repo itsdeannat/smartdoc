53db01cc05c086b17a38b08bf038a0ffcb3f7f21
Deanna Thompson <deanna.thompson45@outlook.com>
2026-02-23 18:45:47 -0500

    fix: allow fail_on to be empty

--- src/smartdoc/analyze.py
+++ src/smartdoc/analyze.py
@@ -27,7 +27,7 @@ def find_file(path: str):
         print(f"{path}")
 
 
-def analyze(file: Annotated[str, typer.Argument(help="Path to the OpenAPI Specification file to be checked")], focus: Annotated[str, typer.Option(help="Restricts the analysis to a specific metric")] = "", fail_on: Annotated[list[str], typer.Option("--fail-on", help="Failure criteria")] = ""):
+def analyze(file: Annotated[str, typer.Argument(help="Path to the OpenAPI Specification file to be checked")], focus: Annotated[str, typer.Option(help="Restricts the analysis to a specific metric")] = "", fail_on: Annotated[list[str], typer.Option("--fail-on", help="Failure criteria")] = None):
     """
     Analyzes an OpenAPI Specification (OAS) file for quality and completeness using an LLM. Users can specify a focus area for the analysis, such as descriptions.
     """
